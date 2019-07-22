754e5402964171925d6037620aff4de8e6c89098
Brian LeQuang <brianlequang@Brians-MacBook-Pro.local>
2019-07-22 17:43:22 -0500

    Base ConvNN working. TODO: replan or hyperparamater tune model

--- neuralNet.py
+++ neuralNet.py
@@ -1,1 +1,51 @@
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
 
+print('Tensorflow version: ' + str(tf.__version__))
+np.set_printoptions(linewidth=150)
+
+# train_images array of 60000 28x28 images
+# train_labels array of 60000 labels for each image
+mnist = tf.keras.datasets.mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+
+def show_image(image):
+    plt.imshow(image, cmap=plt.cm.binary)
+    plt.colorbar()
+    plt.grid(False)
+    plt.show()
+
+train_images = train_images.reshape((60000, 28, 28, 1))
+test_images = test_images.reshape((10000, 28, 28, 1))
+
+# Scale rgb values to a range of 0 to 1 before feeding into network
+train_images = train_images/255.0
+test_images = test_images/255.0
+
+"""# one-hot encoding: reshape labels into array with correct number stored as index
+with tf.compat.v1.Session() as sess:
+    train_labels = sess.run(tf.one_hot(train_labels, depth=10))
+    test_labels = sess.run(tf.one_hot(test_labels, depth=10))
+"""
+
+# placeholder: variable used to feed data into (must match data shape and type exactly)
+x_train = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None, 784])
+y_train = tf.compat.v1.placeholder(dtype=tf.float32, shape=[None, 10])
+image = tf.reshape(x_train, shape=[28,28,1])
+
+model = keras.Sequential()
+model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(tf.keras.layers.MaxPooling2D((2, 2)))
+model.add(tf.keras.layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(64, activation='relu'))
+model.add(tf.keras.layers.Dense(10, activation='softmax'))
+
+print(model.summary())
+
+#model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+#model.fit(train_images, train_labels, epochs=5)
